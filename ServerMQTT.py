df0c7d4f43a0ea78c764e47145ae847aafd38982
Cainã Mota <caina.mota@hotmail.com>
2021-06-18 19:07:47 -0300

    adicionando arquivo de teste para servidor, subscriber que assina e consome os dados
    Signed-off-by: Cainã Mota <caina.mota@hotmail.com>

---
+++ ServerMQTT.py
@@ -0,0 +1,33 @@
+# -*- coding: cp1252 -*-
+import paho.mqtt.client as mqtt
+from struct import unpack
+from time import sleep
+ 
+#config topic
+TOPIC = "/teste"
+ 
+# função chamada quando a conexão for realizada, sendo
+# então realizada a subscrição
+def on_connect(client, data, rc):
+    client.subscribe([(TOPIC,0)])
+ 
+# função chamada quando uma nova mensagem do tópico é gerada
+def on_message(client, userdata, msg):
+    # decodificando o valor recebido
+    v = msg.payload
+    print(v)
+ 
+# clia um cliente para supervisã0
+client = mqtt.Client(client_id = 'SCADA',
+                     protocol = mqtt.MQTTv31)
+
+client.username_pw_set(username = "mqttserver", password = "332451")
+# estabelece as funçõe de conexão e mensagens
+client.on_connect = on_connect
+client.on_message = on_message
+ 
+# conecta no broker
+client.connect("192.168.100.117", 1883)
+ 
+# permace em loop, recebendo mensagens
+client.loop_forever()
